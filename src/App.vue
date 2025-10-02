9f277576e260a1e9e878bad01f822ed1ee567d92
Jonathan Corriveau <joncorv@gmail.com>
2025-10-01 22:42:57 -0700

    tailwind added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "./main.css";
+
+</script>
+
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <div class="w-56 h-56 bg-amber-300 text-5xl"> __ </div>
+
 </template>
 
 <style scoped></style>
