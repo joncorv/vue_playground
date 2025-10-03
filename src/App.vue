5a309b8b9314ab25b47ed83a3bc18f7ac4ba23e1
Jonathan E. Corriveau <joncorv@gmail.com>
2025-10-02 22:43:20 -0700

    for loops over arrays

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,24 @@ import "./main.css";
 
 
 const container = ref(null);
+const mouseStatus = ref(false);
 const relativeX = ref(0);
 const relativeY = ref(0);
-
-
 const percentX = ref(0);
 const percentY = ref(0);
 
-const containerMouseOver = (event) => {
+const containerMouseMove = (event) => {
   const rect = container.value.getBoundingClientRect();
   relativeX.value = event.clientX - rect.left;
   relativeY.value = event.clientY - rect.top;
-
-  // As percentage of element size
   percentX.value = Math.round((relativeX.value / rect.width) * 10000) / 100;
   percentY.value = Math.round((relativeY.value / rect.width) * 10000) / 100;
 }
+
+const containerMouseEnter = () => { mouseStatus.value = true }
+const containerMouseLeave = () => { mouseStatus.value = false }
+
+const notes = [12, 24, 50, 80];
 
 
 </script>
@@ -30,9 +32,34 @@ const containerMouseOver = (event) => {
   <p> relative y: {{ relativeY }} </p>
   <p> percent x: {{ percentX }} </p>
   <p> percent y: {{ percentY }} </p>
+  <p> mouse status: {{ mouseStatus }} </p>
 
-  <div ref="container" @mousemove="containerMouseOver" class="w-56 h-56 bg-amber-300 text-5xl"> </div>
+
+  <div ref="container" @mousemove="containerMouseMove" @mouseenter="containerMouseEnter"
+    @mouseleave="containerMouseLeave" class="flex items-center w-full h-56 bg-amber-300 text-5xl">
+    <div v-for="(note, index) in notes" :key="index" class="note" :style="{ left: `${note}%` }">
+    </div>
+    <div ref="sub-container" class="sub-container w-full h-2 bg-blue-500 "> </div>
+  </div>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.note {
+  position: absolute;
+  width: 10px;
+  height: 20px;
+  background: blue;
+  /* border-radius: 50%; */
+  /* Center the dot on the position */
+  transform: translateX(-50%);
+}
+
+.sub-container {
+  transition: transform 0.3s ease;
+}
+
+.sub-container:hover {
+  transform: scaleY(2);
+}
+</style>
