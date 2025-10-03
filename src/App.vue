410d200b2f2fe956b5303cda8a531da4462b61c1
Jonathan Corriveau <joncorv@gmail.com>
2025-10-03 14:32:59 -0700

    styling up the tooling now

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import "./main.css";
 
 
-const container = ref(null);
+const myContainer = ref(null);
 const mouseStatus = ref(false);
 const relativeX = ref(0);
 const relativeY = ref(0);
@@ -11,7 +11,7 @@ const percentX = ref(0);
 const percentY = ref(0);
 
 const containerMouseMove = (event) => {
-  const rect = container.value.getBoundingClientRect();
+  const rect = myContainer.value.getBoundingClientRect();
   relativeX.value = event.clientX - rect.left;
   relativeY.value = event.clientY - rect.top;
   percentX.value = Math.round((relativeX.value / rect.width) * 10000) / 100;
@@ -35,31 +35,34 @@ const notes = [12, 24, 5, 75, 33, 95, 50, 80];
   <p> mouse status: {{ mouseStatus }} </p>
 
 
-  <div ref="container" @mousemove="containerMouseMove" @mouseenter="containerMouseEnter"
-    @mouseleave="containerMouseLeave" class="flex items-center w-full h-56 bg-amber-300 text-5xl">
+  <div ref="myContainer" class="myContainer" @mousemove="containerMouseMove" @mouseenter="containerMouseEnter"
+    @mouseleave="containerMouseLeave">
     <div v-for="(note, index) in notes" :key="index" class="note" :style="{ left: `${note}%` }">
       <div class="child">{{ index }}</div>
     </div>
     <div ref="sub-container" class="sub-container w-full h-2 bg-blue-500 "> </div>
   </div>
 
+
+
 </template>
 
 <style scoped>
-.note {
-  position: absolute;
-  width: 2px;
-  height: 20px;
-  background: var(--color-blue-500);
-  /* border-radius: 50%; */
-  /* Center the dot on the position */
-  transform: translateX(-50%);
-  transform: translateY(-50%);
-  z-index: 1;
+.myContainer {
+  position: fixed;
+  margin: 1rem;
+  width: calc(100vw - 2rem);
+  height: 8rem;
+  display: flex;
+  background-color: hsl(0, 0%, 25%);
+  align-items: center;
+  border-radius: 25px;
+  box-shadow:
+    inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 2px 4px rgba(0, 0, 0, 0.6);
 }
 
 .child {
-  /* transform: translate(-100%, -100%); */
   transform: translate(-150%, -150%);
   font-size: var(--text-sm);
 }
@@ -71,4 +74,27 @@ const notes = [12, 24, 5, 75, 33, 95, 50, 80];
 .sub-container:hover {
   transform: scaleY(2);
 }
+
+.note {
+  position: absolute;
+  width: 2px;
+  height: 20px;
+  background: var(--color-blue-500);
+  transform: translateX(-50%);
+  transform: translateY(-50%);
+  z-index: 1;
+  margin: 1rem;
+}
+</style>
+
+<style>
+/* body { */
+/*   background-color: hsl(0, 0, 10%); */
+/* } */
+
+/* * { */
+/*   margin: 0; */
+/*   padding: 0; */
+/*   box-sizing: border-box; */
+/* } */
 </style>
